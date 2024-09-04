87f84a7f4c2cc2f56f0a0e010c247cbb1585db01
ThandoMashego <thandomazibukojcele@gmail.com>
2024-09-04 13:55:30 +0200

    Added search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+// seperation of concerns(when we have 2 diffent function that each do something different)
 
+function updateWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#weather-app-city");
+    // why does it matter that the temperatureElement must be at the end
+    cityElement.innerHTML= response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
 
+function searchCity(city){
+    let apiKey = "5ff4aa43fo0e6c0bf35114ad9e37fctc"
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
+    axios.get(apiUrl).then(updateWeather)
+}
 
 function handleSearch(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#weather-app-city");
-    cityElement.innerHTML= searchInput.value;
+    
+    searchCity(searchInput.value);
 }
 
 let searchFormElement= document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch)
+
+searchCity("Cape Town");
